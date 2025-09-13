fc72adbc51f4c300dc8cb5e2cac7f03ab83c6ae3
linga <linga@DESKTOP-47GFIUT>
2025-09-13 09:19:14 +0800

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        CheckBox1 = New CheckBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        RadioButton1 = New RadioButton()
+        GroupBox1 = New GroupBox()
+        RadioButton2 = New RadioButton()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(224, 84)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(85, 19)
+        CheckBox1.TabIndex = 0
+        CheckBox1.Text = "CheckBox1"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(163, 334)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(300, 49)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(230, 284)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Label1"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"BSIT", "BSOA"})
+        ComboBox1.Location = New Point(224, 44)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(200, 23)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(224, 109)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(85, 19)
+        CheckBox2.TabIndex = 0
+        CheckBox2.Text = "CheckBox1"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(224, 134)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(85, 19)
+        CheckBox3.TabIndex = 0
+        CheckBox3.Text = "CheckBox1"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(6, 22)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 19)
+        RadioButton1.TabIndex = 5
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "RadioButton1"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(224, 163)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 100)
+        GroupBox1.TabIndex = 6
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(6, 47)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(97, 19)
+        RadioButton2.TabIndex = 5
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "RadioButton1"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(625, 430)
+        Controls.Add(GroupBox1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim course As String = ""
+        If ComboBox1.SelectedIndex <> -1 Then
+            course = ComboBox1.SelectedItem.ToString()
+        Else
+        End If
+        Dim hobbies As String = ""
+        If CheckBox1.Checked Then
+            hobbies &= "Me"
+        End If
+        If CheckBox2.Checked Then
+            hobbies &= "MySelf"
+        End If
+        If CheckBox3.Checked Then
+            hobbies &= "I"
+        End If
+        If hobbies = "" Then
+            hobbies = "No hobbies selected"
+        End If
+        Dim gender As String = ""
+        If RadioButton1.Checked Then
+            gender = "Male"
+        ElseIf RadioButton2.Checked Then
+            gender = "Female"
+        Else
+            gender = "No gender selected"
+        End If
+        Label1.Text = "Course: " & course & hobbies & gender
+    End Sub
+End Class
